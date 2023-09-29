6f22decdde8ee90547b06e97e28f6ece683364e5
Fritz Lim <fritzlim@gmail.com>
2023-09-30 00:35:04 +0800

    Added navigation button and second events screen

--- screens/EventsScreen.jsx
+++ screens/EventsScreen.jsx
@@ -3,8 +3,36 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 function EventsScreen({ navigation }) {
   return (
-    <View>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "lightgreen",
+      }}
+    >
       <Text>Events first page!</Text>
+      <Button
+        onPress={() => {
+          navigation.navigate("EventsSecond");
+        }}
+        title="Second Screen"
+      ></Button>
+    </View>
+  );
+}
+
+function EventsSecondScreen({ navigation }) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "lightgreen",
+      }}
+    >
+      <Text>Events second page!</Text>
     </View>
   );
 }
@@ -15,6 +43,10 @@ export default function EventsStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="EventsHome" component={EventsScreen}></Stack.Screen>
+      <Stack.Screen
+        name="EventsSecond"
+        component={EventsSecondScreen}
+      ></Stack.Screen>
     </Stack.Navigator>
   );
 }
